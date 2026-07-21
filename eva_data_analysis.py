118ca55f0d4c21fb18460f686e65e4e7188cc84c
benrushscience <rush4@wisc.edu>
2026-07-21 11:29:22 -0500

    adding more comment sections

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -7,12 +7,19 @@ import datetime as dt
 import os
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
+# -----------
+
 
 ### file path variables
-# https://data.nasa.gov/resource/eva.json (with modifications)
+
+# data from https://data.nasa.gov/resource/eva.json (with modifications)
+
 eva_data_json = open('./eva-data.json', 'r')
 eva_data_csv = open('./eva-data.csv','w')
 g_file = 'cumulative_eva_graph.png'
+# -----------
+
 
 ### printing and exporting data
 data=[]
@@ -21,10 +28,13 @@ for i in range(375):
     line=eva_data_json.readline()
     print(line)
     data.append(json.loads(line[1:-1]))
-#data.pop(0)
 
+## export data if needed
+# data.pop(0)
 
 w=csv.writer(eva_data_csv)
+# -----------
+
 
 ### processing time variables
 
@@ -59,6 +69,9 @@ for eva_date, duration_hr in entries:
     date.append(eva_date)
     cumulative_time.append(running_total)
 
+# -----------
+
+
 ### Creating plot of cumulative EVA time
 
 plt.plot(date, cumulative_time, 'ko-')
@@ -67,3 +80,5 @@ plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(g_file)
 plt.show()
+# -----------
+
