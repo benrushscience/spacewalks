81e8ea8c67652ae9fa927b4e5e2a36b51d1f4e71
benrushscience <rush4@wisc.edu>
2026-07-22 10:31:36 -0500

    Add requirement packages and updated test_code fusing pytest

--- test_code.py
+++ test_code.py
@@ -1,5 +1,6 @@
 # test_code.py
 
+import pytest
 from eva_data_analysis import text_to_duration
 
 # convention to use test_ prefix for test functions and then add the name of the function being tested.
@@ -10,12 +11,8 @@ def test_text_to_duration_float():
     assert text_to_duration("10:15") == 10.25
 
 def test_text_to_duration_irrational():
-    print(1e-5)
-    print(text_to_duration("10:20"))
-    print(abs(text_to_duration("10:20") - 10.5))
-    assert abs(text_to_duration("10:20") - 10.5) < 1e-5 #adding some tolerance to account for floating point precision issues
-
-
-test_text_to_duration_integer()
-test_text_to_duration_float()
-test_text_to_duration_irrational()
+    # print(1e-5)
+    # print(text_to_duration("10:20"))
+    # print(abs(text_to_duration("10:20") - 10.333333))
+    # assert abs(text_to_duration("10:20") - 10.333333) < 1e-5 #adding some tolerance to account for floating point precision issues
+    assert text_to_duration("10:20") == pytest.approx(10.333333, rel=1e-5)  # using pytest.approx for floating point comparison
