01f919714ae33fb53755c9c0342c8665b672204c
benrushscience <rush4@wisc.edu>
2026-07-22 11:13:15 -0500

    adding decorator and crew size function

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -8,7 +8,62 @@ import os
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import re
 # -----------
+
+
+
+def main(input_file, output_file, graph_file):
+    print("--START--")
+
+    # Read the data from JSON file
+    eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
+
+    # Convert and export data to CSV file
+    write_dataframe_to_csv(eva_data, output_file)
+
+    # Sort dataframe by date ready to be plotted (date values are on x-axis)
+    eva_data.sort_values('date', inplace=True)
+
+    # Plot cumulative time spent in space over years
+    plot_cumulative_time_in_space(eva_data, graph_file)
+
+    print("--END--")
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 
 def read_json_to_dataframe(input_file):
@@ -107,26 +162,6 @@ def add_duration_hours(df):
 
 print("--END--")
 
-def main():
-    print("--START--")
-
-    input_file = open('./eva-data.json', 'r', encoding='ascii')
-    output_file = open('./eva-data.csv', 'w', encoding='utf-8')
-    graph_file = './cumulative_eva_graph.png'
-
-    # Read the data from JSON file
-    eva_data = read_json_to_dataframe(input_file)
-
-    # Convert and export data to CSV file
-    write_dataframe_to_csv(eva_data, output_file)
-
-    # Sort dataframe by date ready to be plotted (date values are on x-axis)
-    eva_data.sort_values('date', inplace=True)
-
-    # Plot cumulative time spent in space over years
-    plot_cumulative_time_in_space(eva_data, graph_file)
-
-    print("--END--")
 
 
 if __name__ == "__main__":
--- test_code.py
+++ test_code.py
@@ -1,7 +1,11 @@
 # test_code.py
 
 import pytest
-from eva_data_analysis import text_to_duration
+from eva_data_analysis import (
+    text_to_duration,
+    calculate_crew_size,
+)
+
 
 # convention to use test_ prefix for test functions and then add the name of the function being tested.
 def test_text_to_duration_integer():
@@ -16,3 +20,14 @@ def test_text_to_duration_irrational():
     # print(abs(text_to_duration("10:20") - 10.333333))
     # assert abs(text_to_duration("10:20") - 10.333333) < 1e-5 #adding some tolerance to account for floating point precision issues
     assert text_to_duration("10:20") == pytest.approx(10.333333, rel=1e-5)  # using pytest.approx for floating point comparison
+
+# this @pytest is called a decorator, it is used to parametrize the test function with multiple input values and expected results. Could mostly use a loop instead.
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("Valentina Tereskova;", 1),
+    ("Judith Resnik;Sally Ride;", 2),
+    ("", None),
+])
+
+def test_calculate_crew_size(input_value, expected_result):
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
